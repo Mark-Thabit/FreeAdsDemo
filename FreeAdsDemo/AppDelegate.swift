df190ee79511759ebc8ab613d4be987ce43a56e5
Mark Thabit <mark.thabit@hotmail.com>
2023-11-28 07:52:09 +0200

    Initial Commit

---
+++ FreeAdsDemo/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  FreeAdsDemo
+//
+//  Created by Mark Thabit on 28/11/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - iVars
+    
+    var window: UIWindow?
+    
+    // MARK: - App life cycle
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
+    -> Bool {
+        return true
+    }
+}
---
+++ FreeAdsDemo/ViewControllers/CategoryListVC.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryListVC.swift
+//  FreeAdsDemo
+//
+//  Created by Mark Thabit on 28/11/2023.
+//
+
+import UIKit
+
+class CategoryListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
